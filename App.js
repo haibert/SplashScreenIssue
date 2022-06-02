b7b537a3a73f6d010d54e6a81e13fa436590e11b
Haibert Barfian <haibertdude@gmail.com>
2022-06-01 18:56:52 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react'
+import { Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import 'expo-dev-client'
+
+//fonts
+import {
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins'
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
+
+const App = () => {
+    //zustand
+    //-------------------------------------------------------------------------LOAD FONTS AND AUTH STATUS-------------------------------------------------------------------------
+    const [appIsReady, setAppIsReady] = useState(false)
+
+    const loadFonts = useCallback(async () => {
+        try {
+            await SplashScreen.preventAutoHideAsync()
+            await Font.loadAsync({
+                Poppins_300Light,
+                Poppins_400Regular,
+                Poppins_600SemiBold,
+            })
+            setAppIsReady(true)
+        } catch (err) {
+            console.log(err)
+        }
+    }, [])
+    useEffect(() => {
+        loadFonts()
+    }, [])
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            // This tells the splash screen to hide immediately! If we call this after
+            // `setAppIsReady`, then we may see a blank screen while the app is
+            // loading its initial state and rendering its first pixels. So instead,
+            // we hide the splash screen once we know the root view has already
+            // performed layout.
+            await SplashScreen.hideAsync()
+        }
+    }, [appIsReady])
+    //-------------------------------------------------------------------------LOAD FONTS AND AUTH STATUS-------------------------------------------------------------------------
+    if (!appIsReady) {
+        return null
+    }
+
+    return (
+        <View
+            onLayout={onLayoutRootView}
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Text>Test</Text>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
